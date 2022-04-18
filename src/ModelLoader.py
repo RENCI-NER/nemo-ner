b71e92ffb3d1cb777b87b71ad82bad1520cff5f6
Yaphetkg <yaphetkg@renci.org>
2022-04-18 16:27:53 -0400

    making model loader path configurable

--- src/ModelLoader.py
+++ src/ModelLoader.py
@@ -3,7 +3,7 @@ from nemo.collections.nlp.models import TokenClassificationModel
 
 
 def load_model():
-    path = os.path.join(os.path.dirname(__file__), '..', 'trained_model.nemo')
+    path = os.environ.get('MODEL_PATH', os.path.join(os.path.dirname(__file__), '..', 'trained_model.nemo'))
     model = TokenClassificationModel.restore_from(path)
     predictions = model.add_predictions(
         ["ashtma is a disease"]
